c7e8e5caaa5c001dea60964610a643490eddfecb
Denis Kondratev <deniskako2005@gmail.com>
2024-03-09 21:37:53 +0200

    task 3 added.

--- WEB/Lab 2/code/index.php
+++ WEB/Lab 2/code/index.php
@@ -21,9 +21,14 @@ $mypersonalhell = 666.666;
 echo $mypersonalhell; echo "<br>";
 echo 10-5+7; echo "<br>";
 $last_month = 1187.23; $this_month = 1089.98;
-echo $last_month - $this_month; echo "<br>"
+echo $last_month - $this_month; echo "<br>";
 
 #Task 3 - Умножение и деление
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+echo $days_per_language; echo "<br>";
 
 
 
