c1ad8e686cda9406486d9b7a43bea82d9f96feb7
Denis Kondratev <deniskako2005@gmail.com>
2024-03-09 21:30:27 +0200

    task 2 completed; important comment added

--- WEB/Lab 2/code/index.php
+++ WEB/Lab 2/code/index.php
@@ -1,5 +1,7 @@
 <?php
 #Task 1 - Доступ по ссылке
+# Важно: т.к. проверка написанного осуществлялась на странице сайта
+# то вместо \n(который не осуществлял перенос строки) был использован <br>
 
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
@@ -7,7 +9,21 @@ $very_bad_unclear_name = "15 chicken wings";
 
 // Write your code here:
 $order = &$very_bad_unclear_name;
-$order .= ' But I`m vegan =(';
+$order .= ' but I`m vegan =(';
 
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+echo "<br>Your order is: $very_bad_unclear_name.<br>";
+
+#Task 2 - Числа
+$mypersonalheaven = 666;
+echo $mypersonalheaven; echo "<br>";
+$mypersonalhell = 666.666;
+echo $mypersonalhell; echo "<br>";
+echo 10-5+7; echo "<br>";
+$last_month = 1187.23; $this_month = 1089.98;
+echo $last_month - $this_month; echo "<br>"
+
+#Task 3 - Умножение и деление
+
+
+
