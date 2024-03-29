b7ddd231190455e43c1f98ab998cfa7238696508
Denis Kondratev <deniskako2005@gmail.com>
2024-03-29 20:55:14 +0200

    Task 2b added

--- WEB/Lab 3/code/Task2/index2b.php
+++ WEB/Lab 3/code/Task2/index2b.php
@@ -1,1 +1,30 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task 2b</title>
+</head>
+<body>
+    <main>
+        <form action="index2b_session.php" method="POST"> <!-- Форма ввода имени, фамилии, возраста -->
+            <label for="name">
+                Name:
+                <input type="text" name="name" >
+            </label>
+            <label for="surname">
+                Surname:
+                <input type="text" name="surname" >
+            </label>
+            <label for="age">
+                Age:
+                <input type="text" name="age" >
+            </label>
+            <input type="submit" value="Enter">
+        </form>
+    </main>
+</body>
+</html>
+
---
+++ WEB/Lab 3/code/Task2/index2b_session.php
@@ -0,0 +1,31 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Session</title>
+</head>
+<body>
+    <main>
+        <?php
+        if(isset($_POST['name'],$_POST['surname'],$_POST['age']))
+        {
+            $_SESSION['name'] = $_POST['name'];
+            $_SESSION['surname'] = $_POST['surname'];
+            $_SESSION['age'] = $_POST['age'];
+        } else {
+            $_SESSION['name'] = "";
+            $_SESSION['surname'] = "";
+            $_SESSION['age'] = "";
+        }
+        ?>
+        Surname: <?= $_SESSION['surname'] . " " ?><br>
+        Name: <?= $_SESSION['name'] . " " ?><br>
+        Age: <?= $_SESSION['age'] . " "?><br>
+
+    </main>
+</body>
+</html>
